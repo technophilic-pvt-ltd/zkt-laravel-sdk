48cd189e7d3ef53b5092ffb1fe8950368f8597a7
Ravi-arya009 <ravi.arya009@gmail.com>
2025-08-11 17:14:26 +0530

    Revert "added routes and controller to the package."
    This reverts commit 4964cedd1bc6bb22f55d95cdf96feffc5abe725a.

--- src/ZKTecoLaravelSdkServiceProvider.php
+++ src/ZKTecoLaravelSdkServiceProvider.php
@@ -11,16 +11,6 @@ class ZKTecoLaravelSdkServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__ . '/../config/zkteco-laravel-sdk.php' => config_path('zkteco-laravel-sdk.php'),
         ], 'config');
-
-        // Load routes
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-
-        // If you want to publish routes (optional)
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/routes/web.php' => base_path('routes/zkt-laravel-sdk.php'),
-            ], 'routes');
-        }
     }
 
     public function register()
