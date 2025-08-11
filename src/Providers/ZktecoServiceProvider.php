e6d99ba2974306e66d26ea34cf96cd364a6734b5
Ravi-arya009 <ravi.arya009@gmail.com>
2025-08-11 12:13:24 +0530

    updated composer

--- src/Providers/ZktecoServiceProvider.php
+++ src/Providers/ZktecoServiceProvider.php
@@ -1,12 +1,16 @@
 <?php
 
-namespace Technophilic\Zkteco\Providers;
+namespace Technophilic\ZktLaravelSdk\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
 class ZktecoServiceProvider extends ServiceProvider
 {
-    public function boot() {}
+
+    public function boot()
+    {
+        // dd("Zkteco is running");
+    }
 
     public function register() {}
 }
