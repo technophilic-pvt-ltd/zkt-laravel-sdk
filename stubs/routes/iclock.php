3cb12672cf1fd369093809252d722d7acae6b1a7
Ravi-arya009 <ravi.arya009@gmail.com>
2025-08-18 16:51:51 +0530

    removed dependecy on VerifyCsrfToken middleware.

--- stubs/routes/iclock.php
+++ stubs/routes/iclock.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ZKTeco\IClockController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
-Route::get('/iclock', [IClockController::class, 'index'])->name('index');
+Route::get('/iclock', [IClockController::class, 'index'])->name('index')->withoutMiddleware(VerifyCsrfToken::class);
 
-Route::get('/iclock/cdata', [IClockController::class, 'handshake']);
+Route::get('/iclock/cdata', [IClockController::class, 'handshake'])->withoutMiddleware(VerifyCsrfToken::class);
 
-Route::post('/iclock/cdata', [IClockController::class, 'receiveRecords']);
+Route::post('/iclock/cdata', [IClockController::class, 'receiveRecords'])->withoutMiddleware(VerifyCsrfToken::class);
 
-Route::get('/iclock/getrequest', [IClockController::class, 'getrequest']);
+Route::get('/iclock/getrequest', [IClockController::class, 'getrequest'])->withoutMiddleware(VerifyCsrfToken::class);
